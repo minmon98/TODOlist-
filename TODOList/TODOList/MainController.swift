c196cc0c5a76cdbca99c9eb116c065cf83e999c6
Minh Mon <phivanminh10@gmail.com>
2018-07-18 09:46:10 +0700

    create project and initial setup

---
+++ TODOList/TODOList/MainController.swift
@@ -0,0 +1,25 @@
+//
+//  MainController.swift
+//  TODOList
+//
+//  Created by Minh Mon on 7/18/18.
+//  Copyright © 2018 Minh Mon. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
